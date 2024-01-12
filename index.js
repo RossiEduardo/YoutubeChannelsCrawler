275a812f3f0a0eb55ec544528292a32880a9ec52
RossiEduardo <eduardorossi80@hotmail.com>
2024-01-12 17:10:31 -0300

    Improove the script

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function create_csv(yt_channels_data) {
 
 async function login_to_google(page, navigationPromise) {
 	await page.goto('https://accounts.google.com/');
-    console.log("login to google...")
+	console.log('login to google...');
 	await navigationPromise;
 	await page.waitForTimeout(1000);
 
@@ -65,7 +65,7 @@ async function login_to_google(page, navigationPromise) {
 
 	await page.waitForSelector('#passwordNext');
 	await page.click('#passwordNext');
-    console.log("logged in to google account");
+	console.log('logged in to google account');
 }
 
 async function get_channel_name(page) {
@@ -79,25 +79,24 @@ async function get_channel_name(page) {
 }
 
 async function categorize_social_medias(links) {
-    const social_medias = {
-        'others': []
-    };
+	const social_medias = {
+		others: []
+	};
 
-    links.forEach(link => {
-        if (link.includes('facebook.com')) {
-            social_medias.facebook = link;
-        } else if (link.includes('instagram.com')) {
+	links.forEach(link => {
+		if (link.includes('facebook.com')) {
+			social_medias.facebook = link;
+		} else if (link.includes('instagram.com')) {
 			social_medias.instagram = link;
-        } else if (link.includes('twitter.com')) {
+		} else if (link.includes('twitter.com')) {
 			social_medias.twitter = link;
-        } else {
-            social_medias['others'].push(link);
-        }
-    });
+		} else {
+			social_medias['others'].push(link);
+		}
+	});
 
-    return social_medias;
+	return social_medias;
 }
-
 
 async function get_channel_social_medias(page) {
 	await page.click('.yt-simple-endpoint.style-scope.ytd-channel-tagline-renderer');
@@ -114,45 +113,38 @@ async function get_channel_social_medias(page) {
 
 async function get_channel_email(page) {
 	// Click on the button to see the users email
-	const buttonXPath = `//button[contains(., 'Ver endereço de e-mail')]`; //change to the right language
-	await page.waitForXPath(buttonXPath);
-	const [button] = await page.$x(buttonXPath);
-	if (button) {
-		await button.click();
-		// Wait for the reCAPTCHA iframe to appear
-		const iframeSelector = 'iframe[src^="https://www.google.com/recaptcha/"]';
-		await page.waitForSelector(iframeSelector);
-		const frame = await page.$(iframeSelector);
-		const frameContent = await frame.contentFrame();
+	await page.waitForSelector('#view-email-button-container');
+	await page.click('#view-email-button-container');
+	// Wait for the reCAPTCHA iframe to appear
+	const iframeSelector = 'iframe[src^="https://www.google.com/recaptcha/"]';
+	await page.waitForSelector(iframeSelector);
+	const frame = await page.$(iframeSelector);
+	const frameContent = await frame.contentFrame();
 
-		// Switch to the reCAPTCHA iframe
-		await page.waitForTimeout(1000); // Adding a short delay for stability
-		const checkboxSelector = '.recaptcha-checkbox-checkmark';
-		await frameContent.waitForSelector(checkboxSelector, { visible: true });
-		const checkbox = await frameContent.$(checkboxSelector);
-		await checkbox.click();
-		console.log("Resolving captcha...");
-		//wait the captcha be resolved
-		await page.waitForTimeout(15000);
-		//click on the submit button
-		await page.waitForSelector('#submit-btn');
-		await page.click('#submit-btn');
-		//wait the email loads
-		await page.waitForTimeout(2000);
-		//get the email
-		await page.waitForSelector('#email');
-		const email = await page.$eval('#email', element => element.textContent);
-		console.log('e-mail:', email);
-		return email;
-	} else {
-		console.error('Button not found');
-		return '';
-	}
+	// Switch to the reCAPTCHA iframe
+	await page.waitForTimeout(1000); // Adding a short delay for stability
+	const checkboxSelector = '.recaptcha-checkbox-checkmark';
+	await frameContent.waitForSelector(checkboxSelector, { visible: true });
+	const checkbox = await frameContent.$(checkboxSelector);
+	await checkbox.click();
+	console.log('Resolving captcha...');
+	//wait the captcha be resolved
+	await frameContent.waitForSelector('.recaptcha-checkbox-checked')
+	//click on the submit button
+	await page.waitForSelector('#submit-btn');
+	await page.click('#submit-btn');
+	//wait the email loads
+	await page.waitForTimeout(2000);
+	//get the email
+	await page.waitForSelector('#email');
+	const email = await page.$eval('#email', element => element.textContent);
+	console.log('e-mail:', email);
+	return email;
 }
 
 async function scrap_channel_data(channel_url, page) {
 	let yt_channel = {};
-	let social_medias = {}
+	let social_medias = {};
 	yt_channel.url = channel_url;
 	await page.goto(channel_url);
 	await page.waitForTimeout(1000);
@@ -174,43 +166,49 @@ async function scrap_channel_data(channel_url, page) {
 }
 
 async function main() {
-    console.log('Connecting to Scraping Browser...');
-    const browser = await puppeteer.launch({
-        headless: false,
-        args: [`--disable-extensions-except=${pathToExtension}`, `--load-extension=${pathToExtension}`],
-        executablePath: executablePath(),
-    });
-    console.log('Connected!');
-    const page = await browser.newPage();
-    await page.setUserAgent(
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-    );
-    const navigationPromise = page.waitForNavigation();
-    try {
-        let yt_channels_data = [];
-        await login_to_google(page, navigationPromise);
-        await navigationPromise;
+	console.log('Connecting to Scraping Browser...');
+	const browser = await puppeteer.launch({
+		headless: false,
+		args: [`--disable-extensions-except=${pathToExtension}`, `--load-extension=${pathToExtension}`],
+		executablePath: executablePath()
+	});
+	console.log('Connected!');
+	const page = await browser.newPage();
+	await page.setUserAgent(
+		'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+	);
+	const navigationPromise = page.waitForNavigation();
+	try {
+		let yt_channels_data = [];
+		await login_to_google(page, navigationPromise);
+		await navigationPromise;
 
-        const yt_url_channels = await get_yt_urls('yt_channels.txt');
+		const yt_url_channels = await get_yt_urls('yt_channels.txt');
 
-        await page.waitForTimeout(2000);
+		await page.waitForTimeout(2000);
 
-        for (let i = 0; i < yt_url_channels.length; i++) {
-            try {
-                const yt_channel = await scrap_channel_data(yt_url_channels[i], page);
-                yt_channels_data.push(yt_channel);
-            } catch (error) {
-                console.error(`Error scraping channel ${i + 1}:`, error.message);
-            }
-        }
-        create_csv(yt_channels_data);
-    } finally {
-        await page.close();
-        await browser.close();
-    }
+		for (let i = 0; i < yt_url_channels.length; i++) {
+			try {
+				const yt_channel = await scrap_channel_data(yt_url_channels[i], page);
+				yt_channels_data.push(yt_channel);
+				if(yt_channel.email === " "){
+					console.log("Email address hidden. You have reached your access limit for today. Try switching accounts");
+					console.log("Exiting...");
+					break;
+				}
+			} catch (error) {
+				console.error(`Error scraping channel ${i + 1}:`, error.message);
+			}
+		}
+		create_csv(yt_channels_data);
+	} finally {
+		await page.close();
+		await browser.close();
+	}
 }
 
-main().catch((err) => {
-    console.error(err.stack || err);
-    process.exit(1);
+main().catch(err => {
+	console.error(err.stack || err);
+	process.exit(1);
 });
+
